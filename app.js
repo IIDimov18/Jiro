3e644fd0f519ac54e6960bc21d9fec667d0feada
Ivan Dimov <iidimov18@codingburgas.bg>
2021-07-06 23:27:26 +0300

    Implement sidebar with bootstrap 5
    * Add sidebar with layout

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var createError = require('http-errors');
 var express = require('express');
+//const expressLayouts = require('express-ejs-layouts')
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -10,6 +11,8 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 // view engine setup
+//app.use(expressLayouts);
+// app.set('layout', './public/layouts');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
