d65b8ae4afb0576ed3387793bd0eeb64d6eb23f1
IIDimov18 <IIDimov18@codingburgas.bg>
2021-07-08 14:09:07 +0300

    Implement procedure for creating a user

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,13 @@
 var express = require('express');
+const bodyParser = require('body-parser');
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+  res.send('respond with a resource SLIVI 123');
+});
+
+router.post('/', function(req, res, next) {
   res.send('respond with a resource SLIVI 123');
 });
 
