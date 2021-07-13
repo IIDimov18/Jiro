89f78e87658ab740e4744495a8f805d95e7231bd
IIDimov18 <IIDimov18@codingburgas.bg>
2021-07-13 14:51:26 +0300

    Changes to the registerUser procedure

--- database-script/script.sql
+++ database-script/script.sql
@@ -1,6 +1,6 @@
 USE [master]
 GO
-/****** Object:  Database [Jiro]    Script Date: 7/8/2021 2:07:32 PM ******/
+/****** Object:  Database [Jiro]    Script Date: 7/13/2021 2:50:02 PM ******/
 CREATE DATABASE [Jiro]
  CONTAINMENT = NONE
  ON  PRIMARY 
@@ -87,7 +87,7 @@ ALTER DATABASE SCOPED CONFIGURATION SET QUERY_OPTIMIZER_HOTFIXES = OFF;
 GO
 USE [Jiro]
 GO
-/****** Object:  Table [dbo].[Projects]    Script Date: 7/8/2021 2:07:32 PM ******/
+/****** Object:  Table [dbo].[Projects]    Script Date: 7/13/2021 2:50:02 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -106,7 +106,7 @@ CREATE TABLE [dbo].[Projects](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[ProjectsTeams]    Script Date: 7/8/2021 2:07:32 PM ******/
+/****** Object:  Table [dbo].[ProjectsTeams]    Script Date: 7/13/2021 2:50:03 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -116,7 +116,7 @@ CREATE TABLE [dbo].[ProjectsTeams](
 	[TeamId] [int] NOT NULL
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[Tasks]    Script Date: 7/8/2021 2:07:32 PM ******/
+/****** Object:  Table [dbo].[Tasks]    Script Date: 7/13/2021 2:50:03 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -138,7 +138,7 @@ CREATE TABLE [dbo].[Tasks](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[Teams]    Script Date: 7/8/2021 2:07:32 PM ******/
+/****** Object:  Table [dbo].[Teams]    Script Date: 7/13/2021 2:50:03 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -156,7 +156,7 @@ CREATE TABLE [dbo].[Teams](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[TeamsUsers]    Script Date: 7/8/2021 2:07:32 PM ******/
+/****** Object:  Table [dbo].[TeamsUsers]    Script Date: 7/13/2021 2:50:03 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -166,7 +166,7 @@ CREATE TABLE [dbo].[TeamsUsers](
 	[TeamId] [int] NOT NULL
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[Users]    Script Date: 7/8/2021 2:07:32 PM ******/
+/****** Object:  Table [dbo].[Users]    Script Date: 7/13/2021 2:50:03 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -179,7 +179,7 @@ CREATE TABLE [dbo].[Users](
 	[Password] [varchar](128) NOT NULL,
 	[IsAdmin] [bit] NOT NULL,
 	[CreatedAt] [datetime] NOT NULL,
-	[IdOfCreator] [int] NOT NULL,
+	[IdOfCreator] [int] NULL,
 	[LastChangedAt] [datetime] NOT NULL,
 	[IdOfLastChanger] [int] NULL,
 	[Salt] [varchar](69) NOT NULL,
@@ -190,7 +190,7 @@ CREATE TABLE [dbo].[Users](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[WorkLogs]    Script Date: 7/8/2021 2:07:32 PM ******/
+/****** Object:  Table [dbo].[WorkLogs]    Script Date: 7/13/2021 2:50:03 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -208,9 +208,39 @@ CREATE TABLE [dbo].[WorkLogs](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
+SET IDENTITY_INSERT [dbo].[Users] ON 
+GO
+INSERT [dbo].[Users] ([Id], [Username], [FirstName], [LastName], [Password], [IsAdmin], [CreatedAt], [IdOfCreator], [LastChangedAt], [IdOfLastChanger], [Salt], [Token]) VALUES (1, N'TheHolyAdmin', N'BosaNa', N'Kokosa', N'eff1d08060cbda98e6109116bf7e38c4e5d00b19715e9d6a8efdcdabdde6bbd1', 1, CAST(N'2021-07-13T13:47:46.030' AS DateTime), NULL, CAST(N'2021-07-13T13:47:46.030' AS DateTime), NULL, N'solll', N'nqkavToken')
+GO
+INSERT [dbo].[Users] ([Id], [Username], [FirstName], [LastName], [Password], [IsAdmin], [CreatedAt], [IdOfCreator], [LastChangedAt], [IdOfLastChanger], [Salt], [Token]) VALUES (12, N'testKondio', N'Kodio', N'tenten', N'$2b$10$bmpmNZSkhvvhlmOh/bjRB.YB6fqH.9HxNjs/j04Ud3hrSLCbxoxP6', 0, CAST(N'2021-07-13T13:55:34.343' AS DateTime), 1, CAST(N'2021-07-13T13:55:34.343' AS DateTime), NULL, N'$2b$10$bmpmNZSkhvvhlmOh/bjRB.', N'AA597FA5FBAA06B0C128904E7A5EA6')
+GO
+INSERT [dbo].[Users] ([Id], [Username], [FirstName], [LastName], [Password], [IsAdmin], [CreatedAt], [IdOfCreator], [LastChangedAt], [IdOfLastChanger], [Salt], [Token]) VALUES (13, N'testKondio1', N'hhgfhfg', N'gtrfgdf', N'$2b$10$NpLQ.kXVEN87ttghfPyT4e.Rulc2nuEJ/uB12BilhiskAem0LJZt.', 1, CAST(N'2021-07-13T14:08:35.717' AS DateTime), 1, CAST(N'2021-07-13T14:08:35.717' AS DateTime), NULL, N'$2b$10$NpLQ.kXVEN87ttghfPyT4e', N'49155CF2F173E07769533A34D8101A')
+GO
+INSERT [dbo].[Users] ([Id], [Username], [FirstName], [LastName], [Password], [IsAdmin], [CreatedAt], [IdOfCreator], [LastChangedAt], [IdOfLastChanger], [Salt], [Token]) VALUES (14, N'testKondio13', N'hhgfhfg', N'gtrfgdf', N'$2b$10$Q7rGs8epUzZSoQGb8pcU0uxBo3TnqacsVsQawW3NVDCZfMlppaBxG', 1, CAST(N'2021-07-13T14:10:13.860' AS DateTime), 1, CAST(N'2021-07-13T14:10:13.860' AS DateTime), NULL, N'$2b$10$Q7rGs8epUzZSoQGb8pcU0u', N'B4C20B2D5532FAEB6B02A982C83080')
+GO
+INSERT [dbo].[Users] ([Id], [Username], [FirstName], [LastName], [Password], [IsAdmin], [CreatedAt], [IdOfCreator], [LastChangedAt], [IdOfLastChanger], [Salt], [Token]) VALUES (15, N'testKondio131', N'hhgfhfg', N'gtrfgdf', N'$2b$10$ropUr4EEyv4NAYbHcHkgduMeSB7zGdMQIO.bqMxaP8OYnYo8y3aaG', 1, CAST(N'2021-07-13T14:10:34.693' AS DateTime), 1, CAST(N'2021-07-13T14:10:34.693' AS DateTime), NULL, N'$2b$10$ropUr4EEyv4NAYbHcHkgdu', N'04EB1ADB4D176BBDDC2D86D52F781C')
+GO
+INSERT [dbo].[Users] ([Id], [Username], [FirstName], [LastName], [Password], [IsAdmin], [CreatedAt], [IdOfCreator], [LastChangedAt], [IdOfLastChanger], [Salt], [Token]) VALUES (16, N'testKondio1314', N'hhgfhfg', N'gtrfgdf', N'$2b$10$B1UEb1S0BMfAcyZT5ilOWeMB8Dkxdu1olghSRPAtfcVw/42bKEmWO', 1, CAST(N'2021-07-13T14:11:16.540' AS DateTime), 1, CAST(N'2021-07-13T14:11:16.540' AS DateTime), NULL, N'$2b$10$B1UEb1S0BMfAcyZT5ilOWe', N'C371E3A74CBB106DBA817FD3BF5108')
+GO
+INSERT [dbo].[Users] ([Id], [Username], [FirstName], [LastName], [Password], [IsAdmin], [CreatedAt], [IdOfCreator], [LastChangedAt], [IdOfLastChanger], [Salt], [Token]) VALUES (17, N'testKondio13145', N'hhgfhfg', N'gtrfgdf', N'$2b$10$7sYNHVDL2jjx6ba9jiTYG.AI9eFpFfoskKb4t8WVUKGR6RItSWUJe', 1, CAST(N'2021-07-13T14:11:29.510' AS DateTime), 1, CAST(N'2021-07-13T14:11:29.510' AS DateTime), NULL, N'$2b$10$7sYNHVDL2jjx6ba9jiTYG.', N'711FA2B8897E30FAA342B081931378')
+GO
+INSERT [dbo].[Users] ([Id], [Username], [FirstName], [LastName], [Password], [IsAdmin], [CreatedAt], [IdOfCreator], [LastChangedAt], [IdOfLastChanger], [Salt], [Token]) VALUES (18, N'testKondio131455', N'hhgfhfg', N'gtrfgdf', N'$2b$10$Kqq.SlQeCI5qGUcikIHMeeJhQSXWlEiId11nc8RJn8Y5ao3bYAnaG', 1, CAST(N'2021-07-13T14:11:52.507' AS DateTime), 1, CAST(N'2021-07-13T14:11:52.507' AS DateTime), NULL, N'$2b$10$Kqq.SlQeCI5qGUcikIHMee', N'24F39E4D5B49EF7FD93C786E9620E0')
+GO
+INSERT [dbo].[Users] ([Id], [Username], [FirstName], [LastName], [Password], [IsAdmin], [CreatedAt], [IdOfCreator], [LastChangedAt], [IdOfLastChanger], [Salt], [Token]) VALUES (19, N'testKondio134', N'hhgfhfg', N'gtrfgdf', N'$2b$10$fJeN.JSPMvtZpPFKpQkeR.0BJmkl.Ldd14WdP8OFnjSGoad0TQG3G', 1, CAST(N'2021-07-13T14:12:07.253' AS DateTime), 1, CAST(N'2021-07-13T14:12:07.253' AS DateTime), NULL, N'$2b$10$fJeN.JSPMvtZpPFKpQkeR.', N'D392D0301E5BDBD60D4EF5BD0C891E')
+GO
+INSERT [dbo].[Users] ([Id], [Username], [FirstName], [LastName], [Password], [IsAdmin], [CreatedAt], [IdOfCreator], [LastChangedAt], [IdOfLastChanger], [Salt], [Token]) VALUES (20, N'testKondio1341', N'hhgfhfg', N'gtrfgdf', N'$2b$10$pyusdBKhJxp9v4l7U5VfEu/LCDRmKBukLyqDu21rQ0/tGBK.Ra.5C', 1, CAST(N'2021-07-13T14:12:15.060' AS DateTime), 1, CAST(N'2021-07-13T14:12:15.060' AS DateTime), NULL, N'$2b$10$pyusdBKhJxp9v4l7U5VfEu', N'3DF98728D2C0935C09259F3537E77A')
+GO
+INSERT [dbo].[Users] ([Id], [Username], [FirstName], [LastName], [Password], [IsAdmin], [CreatedAt], [IdOfCreator], [LastChangedAt], [IdOfLastChanger], [Salt], [Token]) VALUES (21, N'testKondio13416', N'hhgfhfg', N'gtrfgdf', N'$2b$10$YY7A7TjEytaFvUdsKJxesei2OUig7.4PeMNaBL6tGmGIgJiztQs6u', 1, CAST(N'2021-07-13T14:12:25.910' AS DateTime), 1, CAST(N'2021-07-13T14:12:25.910' AS DateTime), NULL, N'$2b$10$YY7A7TjEytaFvUdsKJxese', N'CA8F3DDBD21CC3B6AD494C10053327')
+GO
+INSERT [dbo].[Users] ([Id], [Username], [FirstName], [LastName], [Password], [IsAdmin], [CreatedAt], [IdOfCreator], [LastChangedAt], [IdOfLastChanger], [Salt], [Token]) VALUES (22, N'testKondio134161', N'hhgfhfg', N'gtrfgdf', N'$2b$10$DkCAxfAW8QqNk7xjOCkZT.7I7R4JzzJXAEwsePqElPPQF71UKemdW', 1, CAST(N'2021-07-13T14:12:50.607' AS DateTime), 1, CAST(N'2021-07-13T14:12:50.607' AS DateTime), NULL, N'$2b$10$DkCAxfAW8QqNk7xjOCkZT.', N'6A1C73A5EE5711FC72E957A7384DB7')
+GO
+INSERT [dbo].[Users] ([Id], [Username], [FirstName], [LastName], [Password], [IsAdmin], [CreatedAt], [IdOfCreator], [LastChangedAt], [IdOfLastChanger], [Salt], [Token]) VALUES (23, N'testKondio1341617', N'hhgfhfg', N'gtrfgdf', N'$2b$10$WFf1lIZf3fVAdV0K0JePFeC7b9GOuJ3y7HTkXE4FRu/.AhxnvK7yW', 1, CAST(N'2021-07-13T14:13:19.567' AS DateTime), 1, CAST(N'2021-07-13T14:13:19.567' AS DateTime), NULL, N'$2b$10$WFf1lIZf3fVAdV0K0JePFe', N'2EB79EFEDE440AC9E98AA9840CB57A')
+GO
+SET IDENTITY_INSERT [dbo].[Users] OFF
+GO
 SET ANSI_PADDING ON
 GO
-/****** Object:  Index [IX_Users]    Script Date: 7/8/2021 2:07:32 PM ******/
+/****** Object:  Index [IX_Users]    Script Date: 7/13/2021 2:50:03 PM ******/
 ALTER TABLE [dbo].[Users] ADD  CONSTRAINT [IX_Users] UNIQUE NONCLUSTERED 
 (
 	[Username] ASC
@@ -310,7 +340,7 @@ REFERENCES [dbo].[Tasks] ([Id])
 GO
 ALTER TABLE [dbo].[WorkLogs] CHECK CONSTRAINT [FK_WorkLogs_Tasks]
 GO
-/****** Object:  StoredProcedure [dbo].[RegisterUser]    Script Date: 7/8/2021 2:07:32 PM ******/
+/****** Object:  StoredProcedure [dbo].[RegisterUser]    Script Date: 7/13/2021 2:50:03 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -339,8 +369,8 @@ BEGIN
 	DECLARE @Token varchar(69)
 	SET @Token = CONVERT(varchar,HASHBYTES('SHA2_256', CONVERT(varchar, GETDATE()-12)+ 'Foncho'+ @Username + CONVERT(varchar, RAND()*(25-10)+10)),2)
 
-	INSERT INTO Users (FirstName,LastName,Username,[Password],IsAdmin,Token,IdOfCreator)
-	VALUES (@FirstName,@LastName,@Username,@Password,@IsAdmin,@Token, @IdOfCreator)
+	INSERT INTO Users (FirstName,LastName,Username,[Password],IsAdmin,Token,IdOfCreator, Salt)
+	VALUES (@FirstName,@LastName,@Username,@Password,@IsAdmin,@Token, @IdOfCreator, @Salt)
 
 	SELECT 0 AS Success
 END
